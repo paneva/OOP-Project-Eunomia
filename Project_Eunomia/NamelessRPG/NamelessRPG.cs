7963ecda51eaebe7598840f365a81bd9d2bc909f
tanerfaikov <tanerfaikov@gmail.com>
2015-06-21 14:44:08 +0300

    First commit.

---
+++ Project_Eunomia/NamelessRPG/NamelessRPG.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Engine;
+
+namespace NamelessRPG
+{
+    public partial class NamelessRPG : Form
+    {
+        private Player _player;
+
+        public NamelessRPG()
+        {
+            InitializeComponent();
+
+
+            _player = new Player(100, 100, 0, 0, 1);
+
+            IblHitPoints.Text = _player.MaximumHitPoints.ToString();
+            IblGold.Text = _player.Gold.ToString();
+            IblExperience.Text = _player.ExperiencePoints.ToString();
+            IblLevel.Text = _player.Level.ToString();
+
+
+        }
+    }
+}
