32cdd8a47f283504ab00d3d71cd78eb3010e4fa8
Rayna Paneva <r.t.paneva@gmail.com>
2015-06-30 09:21:23 +0300

    Change
    Add enum HealingAmount

---
+++ Project_Eunomia/Engine/Items/HealingAmount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Items
+{
+    public enum HealingAmount
+    {
+        Small=50,
+        Medium=100,
+        Big=150
+    }
+}
